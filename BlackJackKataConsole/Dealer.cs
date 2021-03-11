89ac9c86d252e2986b7142193a224cded5960eaa
Leah Filkin <leah.filkin@myob.com>
2021-03-12 09:49:52 +1300

    cleaned up code

--- BlackJackKataConsole/Dealer.cs
+++ BlackJackKataConsole/Dealer.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace BlackJackKataConsole
 {
     public class Dealer
     {
-        public void TellPlayerScore()
+        public Hand AddFirstCardsToHand(Hand hand, Deck deck, int amountOfCards)
         {
+            for (var i = 0; i < amountOfCards; i++)
+            {
+                hand.theirCards.Add(deck.GetRandomCard());
+            }
+            return hand;
         }
     }
 }
--- BlackJackKataConsole/Program.cs
+++ BlackJackKataConsole/Program.cs
@@ -18,84 +18,55 @@ namespace BlackJackKataConsole
             var playersHand = new Hand();
             var dealersHand = new Hand();
             var winChecker = new WinChecker();
-            // give two cards to player
-            // give two cards to dealer
-            var amountOfCardsFirstHand = 2;
-            for (var i = 0; i < amountOfCardsFirstHand; i++)
-            {
-                playersHand.theirCards.Add(deck.GetRandomCard());
-                dealersHand.theirCards.Add(deck.GetRandomCard());
-            }
-            var hitOrStay = "1";
-            var hasStarted = false;                
-            var youArePlaying = true;
+            var dealer = new Dealer();
+            playersHand = dealer.AddFirstCardsToHand(playersHand, deck, 2);
+            dealersHand = dealer.AddFirstCardsToHand(dealersHand, deck, 2);
+            var hasSeenFirstCards = false;
             var playerHandValue = 0;
-            while (hitOrStay == "1") 
+
+            while (true)
             {
-                if (hasStarted)
+                // 'You are currently at {score}'
+                playerHandValue = playersHand.CalculateHandValue();
+                Console.WriteLine($"\nYou are currently at {playerHandValue}");
+                playersHand.PrintProgress(playersHand);
+                if (playerHandValue > 21)
                 {
-                    // Ask to hit or stay
-                    Console.Write("\nHit or stay? (Hit = 1, Stay = 0) ");
-                    hitOrStay = Console.ReadLine();
-                    if (hitOrStay == "0")
-                    {
-                        var dealerHandValue = dealersHand.CalculateHandValue();
-                        var dealerHandMinimum = 17;
-                        var dealersFirstHand = true;
-                        while (dealerHandValue <= dealerHandMinimum || dealerHandValue <= playerHandValue && dealerHandValue > 17)
-                        {
-                            if (!dealersFirstHand)
-                            {
-                                dealersHand.theirCards.Add(deck.GetRandomCard());
-                                //show them what card they got
-                                Console.Write($"\nDealer draws {dealersHand.theirCards.Last()} ");
-                            }
-                            dealersFirstHand = false;
-                            dealerHandValue = dealersHand.CalculateHandValue();
-                            Console.WriteLine($"\nDealer is at {dealerHandValue}");
-                            dealersHand.PrintProgress(dealersHand);
-                            int milliseconds = 3000;
-                            Thread.Sleep(milliseconds);
-                        }
-                        if (dealerHandValue > dealerHandMinimum && dealerHandValue < 21)
-                        {
-                            Console.WriteLine($"\nDealer is at {dealerHandValue}");
-                            dealersHand.PrintProgress(dealersHand);
-                        }
-                        winChecker.PrintOutcomeMessage(dealerHandValue, playerHandValue, dealersHand);
-                        return;
-                    }
-                    // If hit, 
-                    if (hitOrStay == "1")
-                    {
-                        // add card to hand
-                        playersHand.theirCards.Add(deck.GetRandomCard());
-                        //show them what card they got
-                        Console.Write($"You draw {playersHand.theirCards.Last()}");
-                        playerHandValue = playersHand.CalculateHandValue();
-                    }
+                    Console.WriteLine("\nBUST! Better luck next time!");
+                    return;
                 }
-                if (youArePlaying)
+                // Ask to hit or stay
+                Console.Write("\nHit or stay? (Hit = 1, Stay = 0) ");
+                var hitOrStay = Console.ReadLine();
+                if (hitOrStay == "0")
                 {
-                    // 'You are currently at {score}'
-                    hasStarted = true;
-                    playerHandValue = playersHand.CalculateHandValue();
-                    if (playerHandValue > 21)
-                    {
-                        Console.WriteLine($"\nYou are currently at {playerHandValue}");
-                        playersHand.PrintProgress(playersHand);
-                        Console.WriteLine("\nBUST! Better luck next time!");
-                        return;
-                    }
-                    Console.WriteLine($"\nYou are currently at {playerHandValue}");
-                    playersHand.PrintProgress(playersHand);
+                    break;
                 }
-                // If stay, print Dealers score and hand ('dealer is' instead of 'you are')
-                
-                // Dealer draws until they get over 17, then stay unless they get a card low enough to get them closer
-                // to 21
+                // add card to hand
+                playersHand.theirCards.Add(deck.GetRandomCard());
+                //show them what card they got
+                Console.Write($"You draw {playersHand.theirCards.Last()}");
             }
-        }
+            var dealerHandValue = dealersHand.CalculateHandValue();
+            var dealerHandMinimum = 17;
+            while (dealerHandValue <= dealerHandMinimum ||
+                   dealerHandValue <= playerHandValue)
+            {
+                if (hasSeenFirstCards)
+                {
+                    dealersHand.theirCards.Add(deck.GetRandomCard());
+                    //show them what card they got
+                    Console.Write($"\nDealer draws {dealersHand.theirCards.Last()} ");
+                }
+                hasSeenFirstCards = true;
+                dealerHandValue = dealersHand.CalculateHandValue();
+                Console.WriteLine($"\nDealer is at {dealerHandValue}");
+                dealersHand.PrintProgress(dealersHand);
+                int milliseconds = 3000;
+                Thread.Sleep(milliseconds);
+            }
+            winChecker.PrintOutcomeMessage(dealerHandValue, playerHandValue, dealersHand);
         }
     }
+}
 
