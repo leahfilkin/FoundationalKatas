1ccc9dd1977c812a608b831aa53f61d3418f7078
Leah Filkin <leah.filkin@myob.com>
2021-03-18 09:09:06 +1300

    setting up string calculator

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringCalculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ StringCalculatorShould/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace StringCalculatorShould
+{
+    public class Tests
+    {
+        [Fact]
+        public void ReturnANumberWhenAddIsCalled()
+        {
+            //arrange
+            
+            
+            //act
+            
+            //assert
+            Assert.Equal();
+        }
+    }
+}
