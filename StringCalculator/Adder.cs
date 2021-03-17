02730f4a459d7b255193b88d8e853458bfe605bd
Leah Filkin <leah.filkin@myob.com>
2021-03-18 12:09:51 +1300

    adding function for any amount of numbers

--- StringCalculator/Adder.cs
+++ StringCalculator/Adder.cs
@@ -1,10 +1,19 @@
+using System;
+
 namespace StringCalculator
 {
     public class Adder
     {
         public int Add(string input)
         {
-            return 0;
+            if (input == "")
+            {
+                return 0;
+            }
+            else
+            {
+                return Convert.ToInt32(input);
+            }
         }
     }
 }
---
+++ StringCalculatorShould/StringCalculatorShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StringCalculator;
+using Xunit;
+
+namespace StringCalculatorShould
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void ReturnANumberWhenAddIsCalled()
+        {
+            //arrange
+            var adder = new Adder();
+            //act
+            var output = adder.Add("");
+            //assert
+            Assert.Equal(0, output);
+        }
+
+        [Fact]
+        public void ReturnTheSameNumberTheAddFunctionIsGiven()
+        {
+            var adder = new Adder();
+            var number = "5";
+            //act
+            var output = adder.Add(number);
+            //assert
+            Assert.Equal(Convert.ToInt32(number), output);
+        }
+    }
+}
