6b736cb6e504d68a65c737e10520d69f910a659c
Leah Filkin <leah.filkin@myob.com>
2021-04-19 08:52:31 +1200

    run actual simulation

--- MontyHallKata/Program.cs
+++ MontyHallKata/Program.cs
@@ -9,13 +9,14 @@ namespace MontyHallKata
             Console.WriteLine("Welcome to the Monty Hall simulation!");
             var keepOriginalDoor = Strategy.KeepOriginalDoor;
             var changeDoor = Strategy.ChangeDoor;
-            var keepOriginalDoorSimulator = new FakeSimulator(keepOriginalDoor);
-            var changeDoorSimulator = new FakeSimulator(changeDoor);
-            
-            keepOriginalDoorSimulator.CollectGameResults(3);
-            changeDoorSimulator.CollectGameResults(3);
+            var keepOriginalDoorSimulator = new Simulator(keepOriginalDoor);
+            var changeDoorSimulator = new Simulator(changeDoor);
+            keepOriginalDoorSimulator.CollectGameResults(1000);
+            changeDoorSimulator.CollectGameResults(1000);
             var keepResult = keepOriginalDoorSimulator.CalculateResults();
             var changeResult = changeDoorSimulator.CalculateResults();
+            Console.WriteLine($"Keeping the original door results in a {keepResult}% win rate");
+            Console.WriteLine($"Changing the door results in a {changeResult}% win rate");
         }
     }
 }
