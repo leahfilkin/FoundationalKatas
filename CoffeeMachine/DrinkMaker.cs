1e6730280f39cee0d070b4810bc6ee568d2e663d
Leah Filkin <leah.filkin@myob.com>
2021-05-07 14:08:10 +1200

    test passing, refactored v1

--- CoffeeMachine/DrinkMaker.cs
+++ CoffeeMachine/DrinkMaker.cs
@@ -6,42 +6,19 @@ namespace CoffeeMachine
 {
     public class DrinkMaker
     {
-        public string DeliverOrderInformation(string ticket)
+        public string DeliverOrderInformation(string ticketString)
         {
             
-            var ticketInfo = ticket.Split(":");
+            var ticketBreakdown = ticketString.Split(":");
             var instructions = new Dictionary<string, string>()
             {
-                ["drinkInitial"] = ticketInfo[0],
-                ["sugars"] = ticketInfo[1],
-                ["stick"] = ticketInfo[2]
+                ["drinkInitial"] = ticketBreakdown[0],
+                ["sugars"] = ticketBreakdown[1],
+                ["stick"] = ticketBreakdown[2]
             };
-            var drink = "";
-            switch (instructions["drinkInitial"])
-            {
-                case "T":
-                    drink = "tea";
-                    break;
-                case "H":
-                    drink = "chocolate";
-                    break;
-                case "C" :
-                    drink = "coffee";
-                    break;
-            }
-            var sugarAndStickDescription = "";
-            switch (Convert.ToInt32(instructions["sugars"] == "" ? "0" : instructions["sugars"]))
-            {
-                case 0:
-                    sugarAndStickDescription = "no sugar - and therefore no stick";
-                    break;
-                case 1:
-                    sugarAndStickDescription = $"{instructions["sugars"]} sugar and a stick";
-                    break;
-                case 2:
-                    sugarAndStickDescription = $"{instructions["sugars"]} sugars and a stick";
-                    break;
-            }
+            var ticketInstance = new Ticket();
+            var drink = ticketInstance.GetDrinkName(instructions);
+            var sugarAndStickDescription = ticketInstance.GetSugarAndStickDescription(instructions);
             return $"Drink maker makes 1 {drink} with {sugarAndStickDescription}";
         }
     }
---
+++ CoffeeMachine/Ticket.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoffeeMachine
+{
+    public class Ticket
+    {
+        public object GetDrinkName(Dictionary<string, string> instructions)
+        {
+            var drink = "";
+            switch (instructions["drinkInitial"])
+            {
+                case "T":
+                    drink = "tea";
+                    break;
+                case "H":
+                    drink = "chocolate";
+                    break;
+                case "C":
+                    drink = "coffee";
+                    break;
+            }
+
+            return drink;
+        }
+
+        public string GetSugarAndStickDescription(Dictionary<string, string> instructions)
+        {
+            var sugarAndStickDescription = "";
+            switch (Convert.ToInt32(instructions["sugars"] == "" ? "0" : instructions["sugars"]))
+            {
+                case 0:
+                    sugarAndStickDescription = "no sugar - and therefore no stick";
+                    break;
+                case 1:
+                    sugarAndStickDescription = $"{instructions["sugars"]} sugar and a stick";
+                    break;
+                case 2:
+                    sugarAndStickDescription = $"{instructions["sugars"]} sugars and a stick";
+                    break;
+            }
+
+            return sugarAndStickDescription;
+        }
+    }
+}
+    
