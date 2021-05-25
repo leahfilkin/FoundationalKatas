c593106f0d3959ecdb12e6216137fce271a58c0d
Chuan Tan <chuan.tan@myob.com>
2021-05-26 09:15:43 +1000

    Converts up to 10

--- RomanNumerals.Test/RomanNumeralsTests.cs
+++ RomanNumerals.Test/RomanNumeralsTests.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Runtime.InteropServices;
 using Xunit;
 
 namespace RomanNumerals.Test
@@ -11,6 +9,12 @@ namespace RomanNumerals.Test
         [InlineData(2, "II")]
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
         public void ConvertsIntegerToRomanNumeral(int toConvert, string expected)
         {
             var romanNumeral = RomanNumeral.Convert(toConvert);
