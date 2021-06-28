21c4846d9fbcc4a40298e2613beb06f6a7a4536d
Leah Filkin <leah.filkin@myob.com>
2021-06-28 16:10:28 +1200

    trying to read line by line

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Minesweeper.ConsoleClasses;
 
 namespace Minesweeper
@@ -8,6 +9,27 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Minesweeper! Please enter in as many fields as you want. \n" +
+                              "You need to input your field like so: \n" +
+                              "AxB = your field width and height, followed by \n" +
+                              "The field, with * for mines and . for safe spots");
+            string line;
+            var input = new List<List<string>>();
+            var fieldCount = 0;
+            while ((line = Console.ReadLine()) != null && line != "" && line != "00") {
+                if (Char.IsDigit(line[0]))
+                {
+                    input.Add(new List<string> {line});
+                    fieldCount++;
+                }
+                else
+                {
+                    input[fieldCount].Add(line);
+                }
+            }
+            
+            Console.WriteLine(string.Join("\n", input));
+
             var field = new Field(3, 3, 
                 new List<Point> {new Point(0,0), new Point(1,1), new Point(2,2)});
             field.PopulateWithAdjacentMineNumbers();
